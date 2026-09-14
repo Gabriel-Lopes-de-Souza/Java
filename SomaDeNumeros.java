8ef749e0956e2f0e5bf98ffddfa7f6167c774925
Gabriel Lopes de Souza <100160708+Gabriel-Lopes-de-Souza@users.noreply.github.com>
2026-09-14 17:55:31 -0300

    Create SomaDeNumeros.java

---
+++ SomaDeNumeros.java
@@ -0,0 +1,9 @@
+public class SomaDeNumeros {
+    public static void main(String[] args) {
+        int soma = 0;
+        for(int i=1;i<=10;i++){
+            soma += i;
+        }
+        System.out.println("A soma dos números de 1 a 10 é: " +soma);
+    }
+}
